126ad5c53c57026d766110b6ea3408587f080a94
unknown <katammaniteja@gmail.com>
2022-07-16 00:07:54 +0530

    refractored code

--- src/components/Skillcard.js
+++ src/components/Skillcard.js
@@ -4,7 +4,7 @@ export default function Skillcard({skill}) {
     return(
         <div className="col-lg-6">
             <div className="skill-card">
-            <img src={skill.icon} className='skill-card-icon'/>
+            <img src={skill.icon} alt="" className='skill-card-icon my-auto'/>
             <div className="skill-card-body">
                 <h6 className="skill-card-title">
                 {skill.title}
